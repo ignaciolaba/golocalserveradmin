1e892791adbd32d702cd9a6d112f75ff81f6c5ac
iganciolabarca <ignacio@labarca.cl>
2024-07-25 18:25:12 -0400

    route3

--- app/colohouse/getData.ts
+++ app/colohouse/getData.ts
@@ -1,11 +1,21 @@
-export async function getData() {
-    const res = await fetch('/api/route', {
-      method: 'GET',
-    });
+export default async function getData() {
+    try {
+      const response = await fetch('https://core.hivelocity.net/api/v2/account/controlled-client', {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          'x-api-key': process.env.API_KEY as string, // Asegúrate de que la clave de API está definida
+        },
+      });
   
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+  
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error; // Re-lanza el error para que pueda ser manejado en la llamada a getData
     }
-  
-    return res.json();
   }
