2de544cf6603909314eee8d0e72457ca13453ab4
iganciolabarca <ignacio@labarca.cl>
2024-07-25 09:14:10 -0400

    colohouse route

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useEffect, useState } from 'react';
 import styles from './page.module.css';
-import { getData } from './api/colohouse';
+import { getData } from './colohouse/colohouseGetData';
 
 interface ControlledClientData {
   id: string;
