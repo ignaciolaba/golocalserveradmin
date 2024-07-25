cbad393c09f78c3c2b3e064b5056ebe83fbb1b65
iganciolabarca <ignacio@labarca.cl>
2024-07-25 09:08:58 -0400

    next config middleware

--- app/api/colohouse.ts
+++ app/api/colohouse.ts
@@ -4,9 +4,6 @@ export async function getData() {
     headers: {
       accept: 'application/json',
       'x-api-key': process.env.API_KEY as string,
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     },
   });
 
---
+++ app/api/colohouseapi.ts
@@ -0,0 +1,16 @@
+// pages/api/your-endpoint.ts
+
+export default function handler(req: , res) {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Reemplaza '*' con tu dominio en producción
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
+  
+    // Aquí va el resto de la lógica de tu endpoint API
+    res.json({ message: 'Hello World' });
+  }
