7c62307038f9466df3472e470032ed6c9080f269
iganciolabarca <ignacio@labarca.cl>
2024-07-25 08:31:29 -0400

    header methods

--- app/api/colohouse.ts
+++ app/api/colohouse.ts
@@ -4,8 +4,9 @@ export async function getData() {
     headers: {
       accept: 'application/json',
       'x-api-key': process.env.API_KEY as string,
-      'Access-Control-Allow-Origin': 'http://localhost:3000',
-      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     },
   });
 
