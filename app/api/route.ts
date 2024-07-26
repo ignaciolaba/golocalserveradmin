b140f652232d82fa4a24f98aed5bc233d240014a
iganciolabarca <ignacio@labarca.cl>
2024-07-26 12:33:29 -0400

    mcolohouse

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,17 +1,12 @@
-import { NextResponse } from "next/server";
-
-export async function OPTIONS(request: Request) {
-    const allowedOrigin = request.headers.get("origin");
-    const response = new NextResponse(null, {
-      status: 200,
-      headers: {
-        "Access-Control-Allow-Origin": allowedOrigin || "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers":
-          "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
-        "Access-Control-Max-Age": "86400",
-      },
-    });
-  
-    return response;
-  }
+import type { NextApiRequest, NextApiResponse } from 'next'
+ 
+type ResponseData = {
+  message: string
+}
+ 
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  res.status(200).json({ message: 'Hello from Next.js!' })
+}
